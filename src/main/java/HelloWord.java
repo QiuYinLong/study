1f3bc23366ae550ba256a69d6b43604164b6a36e
QiuYinLong <qyl6579138>
2018-03-06 22:32:21 +0800

    测试helloword

---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ * <p/>
+ *
+ * @author Long
+ * @since 2018-3-6  22:19
+ */
+public class HelloWord {
+    public static void main(String[] args){
+        System.out.println("你好");
+    }
+}
